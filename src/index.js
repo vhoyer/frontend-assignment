e17318456edcf8508bcc70f9c4ffb5f4d45009c8
Vinícius Hoyer <vhoyer@live.com>
2019-09-08 12:32:14 -0300

    :recycle: make AppLayout the root layout view for every path
    AppLayout is the former AppView.
    It's nicer to do this way because it enables the ability to add 404
    pages with no header and footer/custom layout.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import 'Styles/main.scss'
 import Vue from 'vue'
-import AppView from './AppView.vue'
 import router from 'Router'
 
 new Vue({
   el: '#app',
-  render: h => h(AppView),
+  render: h => h('router-view'),
   router,
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,12 @@
-import Company from 'Pages/Company';
-import CompanyPage from 'Pages/Company/Page';
-import CompanyData from 'Pages/Company/Data';
+import AppLayout from 'Pages/AppLayout.vue'
+import Company from 'Pages/Company'
+import CompanyPage from 'Pages/Company/Page'
+import CompanyData from 'Pages/Company/Data'
 
 export const routes = [{
   name: 'Home',
   path: '/',
-  component: { render: (h) => h('router-view') },
+  component: AppLayout,
   children: [
     {
       path: 'company',
