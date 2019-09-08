6cf4a06ccfcb25eb871c983df1a6a015c8de7391
Vinícius Hoyer <vhoyer@live.com>
2019-09-08 00:53:55 -0300

    :art: format routes file

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,22 @@
-export const routes = [
-  {
-    name: 'Home',
-    path: '/',
-    children: [
-      {
-        path: 'company',
-        components: require('Pages/Company'),
-        children: [
-          {
-            name: 'Company Page',
-            path: '',
-            // path: '/company/data',
-            components: require('Pages/Company/Page'),
-          },
-          {
-            name: 'Company Data',
-            path: 'data',
-            // path: '/company/data',
-            components: require('Pages/Company/Data'),
-          },
-        ],
-      },
-    ],
-  },
-]
+export const routes = [{
+  name: 'Home',
+  path: '/',
+  children: [
+    {
+      path: 'company',
+      components: require('Pages/Company'),
+      children: [
+        {
+          name: 'Company Page',
+          path: '',
+          components: require('Pages/Company/Page'),
+        },
+        {
+          name: 'Company Data',
+          path: 'data',
+          components: require('Pages/Company/Data'),
+        },
+      ],
+    },
+  ],
+}]
