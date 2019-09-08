3d2a9e3ce10bfa84428dafd0addc1782a00669bf
Vinícius Hoyer <vhoyer@live.com>
2019-09-08 15:13:49 -0300

    :lipstick: make nav on header look nicer

--- src/components/TheHeader/TheHeader.vue
+++ src/components/TheHeader/TheHeader.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="the-header">
-    <nav>
-      <RouterLink
-        to="/company"
-        class="test-company-link"
-      >
-          Company
-      </RouterLink>
-    </nav>
-  </div>
+  <nav class="the-header">
+    <UILink
+      to="/company"
+      class="test-company-link"
+    >
+        Company
+    </UILink>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
 .the-header {
+  display: flex;
+  align-items: center;
+
   height: 5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+
   background-color: var(--color-primary-pure);
+  color: var(--color-neutral-lighter);
 }
 </style>
