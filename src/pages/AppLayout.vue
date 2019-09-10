5f4c5907e2f1e53bbf4167d939dd4d235e8eb4b2
Vinícius Hoyer <vhoyer@live.com>
2019-09-10 08:59:02 -0300

    :recycle: move global style to global stylesheet

--- src/pages/AppLayout.vue
+++ src/pages/AppLayout.vue
@@ -17,25 +17,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-:root {
-  --color-primary-light: #5d699a;
-  --color-primary-pure: #394783;
-  --color-primary-dark: #344279;
-
-  --color-accent: #ff3c6a;
-
-  --color-neutral-white: #ffffff;
-  --color-neutral-lighter: #f3f3f3;
-  --color-neutral-light: #b1b1b1;
-  --color-neutral-pure: #aaaaaa;
-  --color-neutral-dark: #7e7e7e;
-  --color-neutral-darker: #505050;
-}
-
-body {
-  margin: 0;
-  color: var(--color-neutral-darker);
-}
-</style>
