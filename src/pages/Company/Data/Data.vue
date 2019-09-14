8ea54d35d9f60e6ed939a07371238510a01ba8b7
Vinícius Hoyer <vhoyer@live.com>
2019-09-14 16:02:08 -0300

    :bug: re set margins for grid on data page after grid refactor

--- src/pages/Company/Data/Data.vue
+++ src/pages/Company/Data/Data.vue
@@ -7,7 +7,7 @@
       Sit assumenda deleniti ipsum mollitia error. Omnis quaerat error laboriosam deleniti sint Alias aliquam temporibus esse qui quibusdam! Eum blanditiis eligendi iure neque eius. Autem eum omnis tempore nulla eveniet
     </UIText>
 
-    <UIGrid>
+    <UIGrid margins="top">
       <UIInputField
         v-ui:cell="6"
         label="Company Name"
