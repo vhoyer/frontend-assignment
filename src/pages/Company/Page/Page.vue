3ac0e427c64624e8553a357d2161fa28a4d2706d
Vinícius Hoyer <vhoyer@live.com>
2019-09-14 10:49:45 -0300

    :sparkles: add an input to company page

--- src/pages/Company/Page/Page.vue
+++ src/pages/Company/Page/Page.vue
@@ -6,5 +6,9 @@
     <UIText tag="p">
       Sit assumenda deleniti ipsum mollitia error. Omnis quaerat error laboriosam deleniti sint Alias aliquam temporibus esse qui quibusdam! Eum blanditiis eligendi iure neque eius. Autem eum omnis tempore nulla eveniet
     </UIText>
+
+    <UIInputField label="Company Name">
+      <UIInput placeholder="e.g. Your Company Name" />
+    </UIInputField>
   </UICard>
 </template>
