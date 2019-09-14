0d15e44c1697b003cee218b71ab048bf8d74af2c
Vinícius Hoyer <vhoyer@live.com>
2019-09-14 11:59:05 -0300

    :sparkles: make page content match design

--- src/pages/Company/Data/Data.vue
+++ src/pages/Company/Data/Data.vue
@@ -1,5 +1,45 @@
 <template>
-  <p>
-    I'm the data page
-  </p>
+  <UICard
+    v-ui:cell="9"
+    padding="medium"
+  >
+    <UIText tag="p">
+      Sit assumenda deleniti ipsum mollitia error. Omnis quaerat error laboriosam deleniti sint Alias aliquam temporibus esse qui quibusdam! Eum blanditiis eligendi iure neque eius. Autem eum omnis tempore nulla eveniet
+    </UIText>
+
+    <UIGrid>
+      <UIInputField
+        v-ui:cell="6"
+        label="Company Name"
+      >
+        <UIInput placeholder="e.g. Your Company Name" />
+      </UIInputField>
+
+      <UIInputField
+        v-ui:cell="6"
+        v-ui:row="2"
+        label="Company Spend"
+      >
+        <UIInput placeholder="e.g. $150,000" />
+      </UIInputField>
+
+      <UIInputField
+        v-ui:cell="6"
+        v-ui:row="3"
+        label="Company Spend Ability"
+      >
+        <UIInput placeholder="e.g. $150,000 - $300,000" />
+      </UIInputField>
+
+      <UIInputField
+        v-ui:cell="12"
+        label="Notes"
+      >
+        <UITextarea
+          placeholder="e.g. Good Tech Company"
+          rows="10"
+        />
+      </UIInputField>
+    </UIGrid>
+  </UICard>
 </template>
--- src/pages/Company/Page/Page.vue
+++ src/pages/Company/Page/Page.vue
@@ -1,45 +1,5 @@
 <template>
-  <UICard
-    v-ui:cell="9"
-    padding="medium"
-  >
-    <UIText tag="p">
-      Sit assumenda deleniti ipsum mollitia error. Omnis quaerat error laboriosam deleniti sint Alias aliquam temporibus esse qui quibusdam! Eum blanditiis eligendi iure neque eius. Autem eum omnis tempore nulla eveniet
-    </UIText>
-
-    <UIGrid>
-      <UIInputField
-        v-ui:cell="6"
-        label="Company Name"
-      >
-        <UIInput placeholder="e.g. Your Company Name" />
-      </UIInputField>
-
-      <UIInputField
-        v-ui:cell="6"
-        v-ui:row="2"
-        label="Company Spend"
-      >
-        <UIInput placeholder="e.g. $150,000" />
-      </UIInputField>
-
-      <UIInputField
-        v-ui:cell="6"
-        v-ui:row="3"
-        label="Company Spend Ability"
-      >
-        <UIInput placeholder="e.g. $150,000 - $300,000" />
-      </UIInputField>
-
-      <UIInputField
-        v-ui:cell="12"
-        label="Notes"
-      >
-        <UITextarea
-          placeholder="e.g. Good Tech Company"
-          rows="10"
-        />
-      </UIInputField>
-    </UIGrid>
-  </UICard>
+  <p>
+    I'm the page page
+  </p>
 </template>
