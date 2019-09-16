5a3a7e1b235515ab3b1fefa9c7b44ce7b40b67c0
Vinícius Hoyer <vhoyer@live.com>
2019-09-16 09:54:19 -0300

    :bug: correct the use of v-model on input

--- src/ui/UIInput/UIInput.vue
+++ src/ui/UIInput/UIInput.vue
@@ -2,8 +2,24 @@
   <input
     v-bind="$attrs"
     class="ui-input"
-    v-on="$listeners"
+    v-on="listeners"
+    @input="$emit('input', $event.target.value)"
   >
 </template>
+
+<script>
+export default {
+  computed: {
+    listeners() {
+      const listeners = {...this.$listeners};
+
+      delete listeners.input;
+
+      return listeners;
+    },
+  },
+}
+</script>
+
 
 <style lang="scss" src="./UIInput.scss"></style>
