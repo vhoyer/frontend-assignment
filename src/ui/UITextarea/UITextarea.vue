a4ed36768a85548f6fc54b49886f64086eadfaf5
Vinícius Hoyer <vhoyer@live.com>
2019-09-16 09:56:04 -0300

    :bug: fix v-model for textarea

--- src/ui/UITextarea/UITextarea.vue
+++ src/ui/UITextarea/UITextarea.vue
@@ -2,8 +2,23 @@
   <textarea
     v-bind="$attrs"
     class="ui-textarea"
-    v-on="$listeners"
+    v-on="listeners"
+    @input="$emit('input', $event.target.value)"
   />
 </template>
+
+<script>
+export default {
+  computed: {
+    listeners() {
+      const listeners = {...this.$listeners};
+
+      delete listeners.input;
+
+      return listeners;
+    },
+  },
+}
+</script>
 
 <style lang="scss" src="./UITextarea.scss"></style>
